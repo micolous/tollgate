3ec02e32ff3b5ea1911f0635072e76f1a99d5d88
Michael Farrell <micolous+git@gmail.com>
2012-04-20 04:43:57 +0930

    Enable CSRF middleware in devsite.  Added to views that had it missing.

--- tollgate/settings/base.py
+++ tollgate/settings/base.py
@@ -77,10 +77,11 @@ TEMPLATE_LOADERS = (
 )
 
 MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'tollgate.frontend.common.TollgateMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
 	'django.contrib.messages.middleware.MessageMiddleware',
 )
