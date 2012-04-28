micolous/tollgate

commit df1899d2faa86ebdb1c4ece5a76922b7bc45d6fb

Started adding url names and converting hardcoded urls to url tags

tollgate/frontend/urls.py
@@ -35,8 +35,8 @@ ip4portforward_qsd = dict(
 
 urlpatterns = (
 	# login system
-	url(r'^login/$', 'login'),
-	url(r'^logout/$', 'logout'),
+	url(r'^login/$', 'login', name='login'),
+	url(r'^logout/$', 'logout', name='logout'),
 	url(
 		r'^account/password_change/$',
 		password_change,
@@ -45,18 +45,18 @@ urlpatterns = (
 	),
 
 	# captive portal
-	url(r'^internet/login/here/$', 'internet_login_here'),
-	url(r'^internet/login/(?P<mac_address>[\dABCEDFabcdef]{12})/$', 'internet_login'),
+	url(r'^internet/login/here/$', 'internet_login_here', name='internet-login-here'),
+	url(r'^internet/login/(?P<mac_address>[\dA-Fa-f]{12})/$', name='internet-login'),
 	url(r'^internet/disown/(?P<host_id>\d+)/$', 'internet_disown', name='internet-disown'),
-	url(r'^internet/$', 'internet'),
-	url(r'^internet/refresh/$', 'host_refresh_quick'),
-	url(r'^internet/offline/$', 'internet_offline'),
+	url(r'^internet/$', 'internet', name='disown'),
+	url(r'^internet/refresh/$', 'host_refresh_quick', name='host-refres-quick'),
+	url(r'^internet/offline/$', 'internet_offline', name='internet-offline'),
 
-	url(r'^quota/on/$', 'quota_on'),
-	url(r'^quota/off/$', 'quota_off'),
-	url(r'^quota/user-reset/$', 'quota_user_reset'),
-	#(r'^quota/graph.png$', 'quota_graph'),
-	url(r'^quota/$', 'quota'),
+	url(r'^quota/on/$', 'quota_on', name='quota-on'),
+	url(r'^quota/off/$', 'quota_off', name='quota-off'),
+	url(r'^quota/user-reset/$', 'quota_user_reset', name='quota-user-reset'),
+	#(r'^quota/graph.png$', 'quota_graph', name='quota-graph'),
+	url(r'^quota/$', 'quota', name='quota'),
 
 	url(r'^usage/all/on/$', 'usage_all_on'),
 	url(r'^usage/all/really-on/$', 'usage_all_really_on'),
@@ -69,18 +69,18 @@ urlpatterns = (
 	url(r'^usage/(?P<aid>\d+)/coffee/$', 'usage_coffee', name='usage-coffee'),
 	#url(r'^usage/(?P<aid>\d+)/graph.png$', 'usage_graph', name='usage-graph'),
 	url(r'^usage/(?P<aid>\d+)/$', 'usage_info', name='usage-info'),
-	url(r'^usage/$', 'usage'),
-	url(r'^usage/sort/heavy$', 'usage_heavy'),
-	url(r'^usage/sort/speed$', 'usage_speed'),
-	url(r'^usage/sort/morereset$', 'usage_morereset'),
+	url(r'^usage/$', 'usage', name='usage'),
+	url(r'^usage/sort/heavy$', 'usage_heavy', name="usage-heavy"),
+	url(r'^usage/sort/speed$', 'usage_speed', name="usage-speed"),
+	url(r'^usage/sort/morereset$', 'usage_morereset', name='usage-morereset'),
 
-	url(r'^pclist/unowned/$', 'pclist_unowned'),
-	url(r'^pclist/$', 'pclist'),
+	url(r'^pclist/unowned/$', 'pclist_unowned', name='public-unowned'),
+	url(r'^pclist/$', 'pclist', name='pclist'),
 
-	url(r'^captive_landing/?$', 'captive_landing'),
+	url(r'^captive_landing/?$', 'captive_landing', name='captive-landing'),
 
-	url(r'^help/new/$', direct_to_template, dict(template='frontend/help/new.html', extra_context=dict(settings=settings))),
-	url(r'^help/api/$', direct_to_template, dict(template='frontend/help/api.html', extra_context=dict(settings=settings))),
+	url(r'^help/new/$', direct_to_template, dict(template='frontend/help/new.html', extra_context=dict(settings=settings)), name='help-new'),
+	url(r'^help/api/$', direct_to_template, dict(template='frontend/help/api.html', extra_context=dict(settings=settings)), name='help-api'),
 
 	# signin system
 	url(r'^signin/$', 'signin1', name='signin'),

tollgate/frontend/templates/frontend/base-internet.html
@@ -1,4 +1,4 @@
-{% extends "frontend/base.html" %}
+{% extends "frontend/base.html" %}{% load future %}
 {% comment %}
 tollgate base internet page
 Copyright 2008-2010 Michael Farrell <http://micolous.id.au/>
@@ -23,7 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 {% block submenu %}
 	<!-- $Id: base-internet.html 109 2010-11-10 12:23:25Z michael $ -->
 	<ul>
-		<li><a href="/internet/login/here/">{% trans "enable internet connectivity for this pc" %}</a></li>
+		<li><a href="{{ url "internet-login-here" }}">{% trans "enable internet connectivity for this pc" %}</a></li>
 	</ul>
 {% endblock %}
 

tollgate/frontend/templates/frontend/base-pclist.html
@@ -1,4 +1,4 @@
-{% extends "frontend/base.html" %}
+{% extends "frontend/base.html" %}{% load future %}
 {% comment %}
 tollgate base pc list page
 Copyright 2008-2010 Michael Farrell <http://micolous.id.au/>
@@ -24,8 +24,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 {% block submenu %}
 <!-- $Id: base-pclist.html 109 2010-11-10 12:23:25Z michael $ -->
 <ul>
-	<li><a href="/pclist/">{% trans "owned pcs" %}</a></li>
-	<li><a href="/pclist/unowned/">{% trans "unowned pcs" %}</a></li>
+	<li><a href="{% url "pclist" %}">{% trans "owned pcs" %}</a></li>
+	<li><a href="{% url "pclist-unowned" %}">{% trans "unowned pcs" %}</a></li>
 </ul>
 {% endblock %}
 

tollgate/frontend/templates/frontend/base-usage.html
@@ -1,4 +1,4 @@
-{% extends "frontend/base.html" %}
+{% extends "frontend/base.html" %}{% load future %}
 {% comment %}
 tollgate base usage page
 Copyright 2008-2010 Michael Farrell <http://micolous.id.au/>
@@ -25,10 +25,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  -->
 
 <ul>
-	<li><a href="/usage/">{% trans "internet usage report" %}</a></li>
-	<li><a href="/usage/sort/heavy">{% trans "highest quota use" %}</a></li>
-	<li><a href="/usage/sort/morereset">{% trans "most resets" %}</a></li>
-	<li><a href="/usage/sort/speed">{% trans "highest speed" %}</a></li>
+	<li><a href="{% url "usage" %}">{% trans "internet usage report" %}</a></li>
+	<li><a href="{% url "usage-heavy" %}">{% trans "highest quota use" %}</a></li>
+	<li><a href="{% url "usage-morereaset" %}">{% trans "most resets" %}</a></li>
+	<li><a href="{% url "usage-speed" %}">{% trans "highest speed" %}</a></li>
 </ul>
 {% endblock %}
 

tollgate/frontend/templates/frontend/base.html
@@ -1,5 +1,5 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">{% load future %}
 {% comment %}
 tollgate base template page
 Copyright 2008-2012 Michael Farrell <http://micolous.id.au/>
@@ -20,15 +20,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 {% load i18n %}
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="{{ LANGUAGE_CODE }}" lang="{{ LANGUAGE_CODE }}">
 	<head>
-		<script language="javascript" type="text/javascript" src="{{ STATIC_URL }}tollgate/flot/jquery.min.js"></script> 
-		<script language="javascript" type="text/javascript" src="{{ STATIC_URL }}tollgate/flot/jquery.flot.min.js"></script> 
+		<script language="javascript" type="text/javascript" src="{{ STATIC_URL }}tollgate/flot/jquery.min.js"></script>
+		<script language="javascript" type="text/javascript" src="{{ STATIC_URL }}tollgate/flot/jquery.flot.min.js"></script>
 
 		{% if user.is_authenticated %}
 			<link rel="stylesheet" href="{{ STATIC_URL }}tollgate/{{ user.get_profile.theme }}.css" media="screen" type="text/css" />
 		{% else %}
 			<link rel="stylesheet" href="{{ STATIC_URL }}tollgate/cake.css" media="screen" type="text/css" />
 		{% endif %}
-		
+
 		{% for f,n in themes %}
 			<link rel="alternate stylesheet" href="{{ STATIC_URL }}tollgate/{{ f }}.css" media="screen" title="{{ n }}" type="text/css" />
 		{% endfor %}
@@ -36,7 +36,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 		<!--
 			tollgate, a captive portal system for lan parties.
 			Copyright 2008-2012 Michael Farrell <http://micolous.id.au/>
-			
+
 			This program is free software: you can redistribute it and/or modify
 			it under the terms of the GNU Affero General Public License as published by
 			the Free Software Foundation, either version 3 of the License, or
@@ -49,7 +49,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 			You should have received a copy of the GNU Affero General Public License
 			along with this program.  If not, see <http://www.gnu.org/licenses/>.
-			
+
 			Source Code URL: {{ settings.SOURCE_URL }}
 		-->
 	</head>
@@ -57,39 +57,39 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 	<body>
 		<!-- hack for background to allow border-image -->
 		<div id="bghack"></div>
-		
+
 		<div id="menubar">
 			<ul>
-				<li><a href="{% url index %}">tollgate</a></li>
-				<li><a href="/quota/">{% trans "my devices and quota" %}</a></li>
-				<li><a href="/internet/">{% trans "login other computers" %}</a></li>
+				<li><a href="{% url 'index" %}">tollgate</a></li>
+				<li><a href="{% url "quota" %}">{% trans "my devices and quota" %}</a></li>
+				<li><a href="{% url "internet" %}">{% trans "login other computers" %}</a></li>
 
 				{% if perms.frontend.can_view_quota %}
-					<li><a href="/usage/">{% trans "quota management" %}</a></li>
+					<li><a href="{% url "usage" %}">{% trans "quota management" %}</a></li>
 				{% endif %}
-				
+
 				{% if perms.frontend.can_view_ownership %}
-					<li><a href="/pclist/">{% trans "computer list" %}</a></li>
+					<li><a href="{% url "pclist" %}">{% trans "computer list" %}</a></li>
 				{% endif %}
-				
+
 				{% if perms.frontend.can_register_attendance %}
 					<li><a href="{% url signin %}">{% trans "sign in" %}</a></li>
 				{% endif %}
-				
+
 				{% if perms.frontend.can_ip4portforward %}
 					<li><a href="{% url ip4portforward_list %}">{% trans "ipv4 port forwarding" %}</a></li>
 				{% endif %}
 				
 				{% if user.is_staff %}
-					<li><a href="/admin/">{% trans "system administration" %}</a></li>
+					<li><a href="{% url "admin:index" %}">{% trans "system administration" %}</a></li>
 				{% endif %}
 				
 				{% if user.is_authenticated %}
 		 			<li>
 	 					{% trans "logged in as:" %} {{ user.username }} ({{ user.first_name }} {{ user.last_name }})
 		 			</li>
-					<li><a href="{% url account_password_change %}">{% trans "change password" %}</a></li>
-		 			<li><a href="/logout/">{% trans "logout" %}</a></li>
+					<li><a href="{% url "account_password_change" %}">{% trans "change password" %}</a></li>
+		 			<li><a href="{% url "logout" %}">{% trans "logout" %}</a></li>
 		 		{% else %}
 		 			<li>{% trans "not logged in" %}</li>
 		 			<li><a href="/login/">{% trans "login" %}</a></li>

tollgate/frontend/templates/frontend/captive_landing.html
@@ -1,4 +1,4 @@
-{% extends "frontend/base.html" %}
+{% extends "frontend/base.html" %}{% load future %}
 {% comment %}
 tollgate captivity landing page
 Copyright 2008-2010 Michael Farrell <http://micolous.id.au/>
@@ -26,54 +26,54 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 {% if user.is_authenticated %}
 	<!-- user is logged in -->
 	<p>Your connection to this internet has been denied.  This is just a guess, but based on what I can see:</p>
-	
+
 	{% if reason_quota %}
-		<p><strong>Quota:</strong> You have exceeded your <a href="/quota/">quota allocation</a> for this event.  If you haven't already, you can get one free reset of your quota, and gain internet access again.  However no additional resets will be available after this.</p>
+		<p><strong>Quota:</strong> You have exceeded your <a href="{% url "quota" %}">quota allocation</a> for this event.  If you haven't already, you can get one free reset of your quota, and gain internet access again.  However no additional resets will be available after this.</p>
 	{% endif %}
 	
 	{% if reason_disabled %}
-		<p><strong>Disabled Internet Access:</strong> You have disabled your internet access.  During an emergency situation, administrators may also disable internet access temporarily.  If there isn't an emergency issue, you can <a href="/quota/on/">click here to re-enable it.</a></p>
+		<p><strong>Disabled Internet Access:</strong> You have disabled your internet access.  During an emergency situation, administrators may also disable internet access temporarily.  If there isn't an emergency issue, you can <a href="{% url "quota-on" %}">click here to re-enable it.</a></p>
 	{% endif %}
-	
+
 	{% if reason_sync %}
-		<p><strong>Synchronisation:</strong> You've previously claimed this computer as being yours and signed in for this event, however your computer is presently marked as being offline.  Every 10 minutes, tollgate disables internet access to computers that have been switched off, and enables internet access to those that have been switched on.  You can either wait ten minutes or <a href="/internet/login/here/">turn on internet access for this computer right now</a>.</p>
+		<p><strong>Synchronisation:</strong> You've previously claimed this computer as being yours and signed in for this event, however your computer is presently marked as being offline.  Every 10 minutes, tollgate disables internet access to computers that have been switched off, and enables internet access to those that have been switched on.  You can either wait ten minutes or <a href="{% url "internet-login-here" %}">turn on internet access for this computer right now</a>.</p>
 	{% endif %}
-	
+
 	{% if reason_login %}
-		<p><strong>Not Owned:</strong> This computer has not been marked as being owned by anyone.  You're currently logged in, so if you wish to claim ownership of this computer and sign it in to the internet in your name, you can <a href="/internet/login/here/">do so now</a>.</p>
+		<p><strong>Not Owned:</strong> This computer has not been marked as being owned by anyone.  You're currently logged in, so if you wish to claim ownership of this computer and sign it in to the internet in your name, you can <a href="{% url "internet-login-here" %}">do so now</a>.</p>
 	{% endif %}
-		
+
 	<h2>Other reasons for connection problems:</h2>
-	
+
 	<ul>
 		<li>
 			<p>You're not signed onto the internet from this computer and IP address.  This may occur if your IP address has changed for some reason.  This can also be caused by synchonisation problems.</p>
 			<ul>
-				<li><a href="/internet/login/here/">Click here to log this computer into the internet</a> to enable internet connectivity for this computer in your name.</li>
+				<li><<a href="{% url "internet-login-here" %}">>Click here to log this computer into the internet</a> to enable internet connectivity for this computer in your name.</li>
 			</ul>
 		</li>
-		
+
 		<li>
 			<p>You have disabled internet access for yourself.</p>
 			<ul>
-				<li><a href="/quota/on/">Click here to enable internet access for yourself.</a></li>
+				<li><a href="{% url "quota-on" %}">Click here to enable internet access for yourself.</a></li>
 			</ul>
 		</li>
-		
+
 		<li>
 			<p>You have run out of quota.</p>
 			<ul>
-				<li><a href="/quota/">Click here to see how much quota you have used.</a></li>
+				<li><a href="/{% url "quota" %}">Click here to see how much quota you have used.</a></li>
 			</ul>
 		</li>
-		
+
 		<li>
 			<p>You haven't logged onto portal and been registered as attending the event.</p>
 			<ul>
-				<li><a href="/login/">Click here to login to tollgate</a></li>
+				<li><a href="{% url "quota-on" %}">Click here to login to tollgate</a></li>
 			</ul>
 		</li>
-		
+
 		<li>
 			<p>The website you are trying to visit is blacklisted.</p>
 			<ul>
@@ -86,12 +86,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 {% else %}
 	<!-- user is not logged in -->
 	<p>Please login with your username and password to access the internet.</p>
-	
-	<form method="post" action="/login/">
+
+	<form method="post" action="{% url "login" %}">
 		{{ f.as_p }}
 		<p><input type="submit" value="Login"></p>
 	</form>
-	
+
 	{% if dest %}
 		<p>If you have already logged in on one browser tab/window, then you can <a href="{{ dest }}">click here to retry accessing the website</a> you were trying to get to.  Unfortunately, saved browser sessions totally break in all captive portals, and there isn't a standard way around this yet.</p>
 	{% endif %}

tollgate/frontend/templates/frontend/help/new.html
@@ -96,7 +96,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 <h2 id="2.6.6">Changes in 2.6.6 (November 2009)</h2>
 <ul>
 	<li>LANdit backend also grabs whether a user has ordered unlimited coffee.</li>
-	<li><code>coffee_ip</code> <a href="/help/api/">API call</a> added.</li>
+	<li><code>coffee_ip</code> <a href="{% url "help-api" %}">API call</a> added.</li>
 	<li>Added option to manually change whether a user is allowed to use the coffee notification system, and extra ACL added to determine whether an administrator is allowed to change that value.</li>
 	<li>Internet connectivity is no longer switched on on login <strong>if</strong> you have previously disabled internet connectivity and haven't selected to sign the current computer on in your name.</li>
 	<li>Backend not running will no longer cause EventAttendance migration failure on login.</li>
@@ -154,7 +154,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 	<li>Resynced the two versions of v2.5 of the code in use.</li>
 	<li>When there is an external authentication failure (such as attendance not registered, or forum password change) on an already-migrated account, you are no longer kept logged in.</li>
 	<li>Offline hosts are now marked as being offline properly.</li>
-	<li>Added <a href="/help/api/">API</a> for interacting with tollgate.</li>
+	<li>Added <a href="{% url "help-api" %>API</a> for interacting with tollgate.</li>
 	<li>Version numbering changed</li>
 </ul>
 

commit 7995883776f845851c26756e3fac50debc540e52

Fix URL router error for "internet_login" view @twoolie introduced in df1899d2fa

tollgate/frontend/urls.py
@@ -46,7 +46,7 @@ urlpatterns = (
 
 	# captive portal
 	url(r'^internet/login/here/$', 'internet_login_here', name='internet-login-here'),
-	url(r'^internet/login/(?P<mac_address>[\dA-Fa-f]{12})/$', name='internet-login'),
+	url(r'^internet/login/(?P<mac_address>[\dA-Fa-f]{12})/$', 'internet_login', name='internet-login'),
 	url(r'^internet/disown/(?P<host_id>\d+)/$', 'internet_disown', name='internet-disown'),
 	url(r'^internet/$', 'internet', name='disown'),
 	url(r'^internet/refresh/$', 'host_refresh_quick', name='host-refres-quick'),