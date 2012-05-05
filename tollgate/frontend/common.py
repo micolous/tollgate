207d14aa58e5704237b6bec79fcc5f587abd273d
Michael Farrell <micolous+git@gmail.com>
2012-05-05 15:49:33 +0930

    reticulate splines.

--- tollgate/frontend/common.py
+++ tollgate/frontend/common.py
@@ -50,19 +50,19 @@ class TollgateMiddleware:
 
 		if u == None:
 			# Fbhepr HEY jnf abg cebivqrq / vainyvq. :(
-			# Vafreg n zrffntr erzvaqvat nqzvavfgengbe gb frg vg va frggvatf_ybpny.cl
+			# Vafreg n zrffntr erzvaqvat nqzvavfgengbe gb frg vg va frggvatf.cl
 			response.write(b64decode("""
-				QlpoOTFBWSZTWbKzEigAAFNfgAAwcAP2HyoF3iC/79/wQAGy5s0waaRGptJg1PVP
-				aJMjyQGgPUNpBomjE1Mimho0MgA2oNGhoMBoDQAAaaDQDQBoEoRE0eo0BoNA00AD
-				QGgwNBBhiC9/r4cec+G8NpoD1S223I9ZO432+4RczvQ4QMpuIZ+CTw9q4Ho/bzxG
-				6DyfQoVLKpcSTudjpKtcc6Zml6qsMwBl3S0BGRSyxr9Ojdt66w6S3MBPMOy5Zhb6
-				ZqQw7ZsCOTWznDCx9zmKNUWvIOCo9j6ilkp48SWKj5opeWTBrsNqjkBax+WSvs0q
-				OO243wcPjPCdEb5OxmFBuYRGgAJAyk1YQmpgMu3bGpdIWyZ1b+egTFcMZ6I0+zLn
-				83LcXW1FN9rmC9I4W2itBNZg69ksO3U0EVSUNKb24pKeAMaBCT0SQDSGB5Ey37jd
-				UziMdZx2TNIUXSchbxSRJcaMVUNSjPUfMLCIrmQghABQkFDwjFjEOrUbBPdCPUuK
-				bQrnivgnBKXtIWCFNdOqfukg6jFCwhQQxlFhUmnIuYJzgHFCeXFdkXsrXwkJjOa3
-				WhWIzhbSGvuWvPIAbHxMjQFGxrlxmCwUmD3oIoBIMCXpYGEopkKf4u5IpwoSFlZi
-				RQA=
+				QlpoOTFBWSZTWWE2GxwAAFPfgAAwcAf2HyoF3iC/79/wQAG9trWANRojUwJ6Jpmk
+				0GCNGgNDagaqfoNE9NSZNGgAAAAAGNDQ0AGQ0AAAAAEppExNBSexU3qekag0BkZo
+				0NCDJih0DeHn40/iXPkT6Yi2fuv4wpe9KHK/0rCXfhYqxEDKISFf1rl5zSKr5O6g
+				dkKiLw9lSmYgWxxzkpPZFI7oQ4k8pERXAC2unIHFpzRxpfyzTDOTZM2SoBz0BVl3
+				aLc3V+Az/PzyaDfR4QHFLxO8TPxK+bEA4kPQXEQxwwEV666rEh4S73S3O/THaEs0
+				N7FpQO944wICXR6NiYu5KRAwV6Zgc6MgA2QpZBJNsQQ1FNe+T6w9W8In+RwGpJlL
+				r00GKJH+iikOpcRzbFqQJzTMWsRmDmlip0UMqarUYauRTb47NrLIxfSplrcNqEUQ
+				LkWP02rdhw0W3PeQjtMx2La5zsa95CQcEPFEdBVsOejFzXAWtucqxB88yeLlCXy3
+				Z+6mmvHnUx6262urFD5Mm5+Xu3Ly1J27MoIcRCFvQnSu+qnAkuommS+/Y9NKwtt7
+				XIndGWErGvZKsr3Oz7St1cDhTFCgNXhPBskNdJpiphYViDkxpb5iIWSTFnwLuSKc
+				KEgwmw2OAA==
 			""").decode('bz2'))
 		else:
 			# Fbhepr HEY jnf cebivqrq.  Vafreg n UGGC urnqre jvgu gur HEY va pnfr
